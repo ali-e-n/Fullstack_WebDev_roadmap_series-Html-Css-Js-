656feab89e938b8b4b93c614bab538af62feb956
Ali Akbar Khan <aliakbarkhan@makethehack.com>
2023-10-25 18:26:17 +0000

    new file

---
+++ JavaScript/25-CodeChallenges/challenge4.js
@@ -0,0 +1,14 @@
+// Function for Counting the number of Vowels in an input String
+
+
+
+function vowelCount (str){
+    
+    let count = 0;
+    strArray = str.split('');
+
+
+//testing function
+
+// Testing function
+console.log(vowelCount("Hi! I am Ali")); // Output: 3
